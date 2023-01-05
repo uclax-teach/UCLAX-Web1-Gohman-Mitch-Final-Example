90168bca929499a19eb0c5bb6230642bab3376ea
Mitch Gohman <meatch@me.com>
2023-01-04 21:57:28 -0700

    Changing Vite to output build instead of dist to support github pages

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import * as path from 'path';
-import svgr from "vite-plugin-svgr";
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
-
     return {
-        plugins: [svgr(),react()],
+        plugins: [svgr(), react()],
         server: {
             port: 3010,
         },
         resolve: {
             alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
         },
+        build: {
+            outDir: './build',
+        },
     };
 });
-
